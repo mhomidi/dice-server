6ffbc36f1f2cfe00766f894a376865986dded679
Hadi Omidi <homidi@uwaterloo.ca>
2023-02-24 13:58:13 -0500

    Move source files to src directory

--- src/kernel_handler.hpp
+++ src/kernel_handler.hpp
@@ -6,11 +6,11 @@
 class KernelHandler
 {
 public:
-    static KernelHandler* getInstance();
+    static KernelHandler *getInstance();
     void addSource(std::tuple<std::string, std::string> key, std::string source);
     void setKernelArgument(std::tuple<std::string, std::string> kernelKey, std::tuple<std::string, size_t> argKey);
     void setWorkDim(std::tuple<std::string, std::string> kernelKey, size_t dim);
-    void setGlobalWorkSize(std::tuple<std::string, std::string> kernelKey, std::vector<size_t>& globalWorkSize);
+    void setGlobalWorkSize(std::tuple<std::string, std::string> kernelKey, std::vector<size_t> &globalWorkSize);
     void clearKernel(std::tuple<std::string, std::string> kernelKey);
     void enqueueKernel(std::tuple<std::string, std::string> key);
 
@@ -19,13 +19,12 @@ public:
     std::string getKernelName(std::tuple<std::string, std::string> key);
     std::vector<std::tuple<std::string, size_t>> getKernelArgument(std::tuple<std::string, std::string> kernelKey);
     size_t getWorkDim(std::tuple<std::string, std::string> kernelKey);
-    size_t* getGlobalWorkSize(std::tuple<std::string, std::string> kernelKey);
+    size_t *getGlobalWorkSize(std::tuple<std::string, std::string> kernelKey);
     size_t getReadyQueueSize();
-
 
 private:
     KernelHandler();
-    static KernelHandler* instance;
+    static KernelHandler *instance;
 
     std::map<std::tuple<std::string, std::string>, std::string> kernelSources;
     std::queue<std::tuple<std::string, std::string>> readyKernelQueue;
@@ -33,5 +32,4 @@ private:
     std::map<std::tuple<std::string, std::string>, std::vector<std::tuple<std::string, size_t>>> kernelsArguments;
     std::map<std::tuple<std::string, std::string>, std::vector<size_t>> kernelGlobalWorkSize;
     std::map<std::tuple<std::string, std::string>, size_t> kernelWorkDim;
-
 };
